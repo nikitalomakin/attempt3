3a75062143bd4d68e7fe75892f5758bc5c292af7
nikitalomakin <91279723+nikitalomakin@users.noreply.github.com>
2021-09-24 18:49:32 +0300

    Update DZ1.c
    Исправил программу

--- DZ1.c
+++ DZ1.c
@@ -1,16 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <ctype.h>
 int main() {
     printf("Введите число с плавающей точкой\n");
     char c;
+	 _Bool dot = 0;
     while ((c = getchar()) != '\n') {
         if (c == '.') {
             printf("|");
         } else if ( ((c > '9') || (c < '0'))) {
             printf("НАдо вводить цифры\n");
             return 0;
-        } else printf("%c",c);
-
+        } else{
+		printf("%c",c);
+	}
+	if (!dot) {
+        	printf("число введено без точки\n");
+        return -1;
     }
     printf("\n");
-} 
+}
+ 
